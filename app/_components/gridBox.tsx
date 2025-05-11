70950683dc82558de2305634517c79501230f515
Hardik Jain <hardik.jain@nutanix.com>
2025-05-11 17:20:15 +0530

    Started about me page

--- app/_components/gridBox.tsx
+++ app/_components/gridBox.tsx
@@ -4,10 +4,11 @@ import Link from "next/link";
 
 interface IGridBoxProps {
   href?: string;
+  className?: string;
 }
 
-function GridBox({ children, href }: PropsWithChildren<IGridBoxProps>) {
-  let boxContent = <div className="relative bg-background h-full shadow-box rounded-3xl">
+function GridBox({ children, href, className }: PropsWithChildren<IGridBoxProps>) {
+  let boxContent = <div className={`relative bg-background h-full shadow-box rounded-3xl w-fit ${className || ""}`}>
     <div className="flex flex-col gap-4 p-8 h-full">
       { children }
     </div>
--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,6 +1,51 @@
+import GridBox from "../_components/gridBox";
+import AboutProfile from "../../public/img/aboutProfile.png";
+import Image from "next/image";
+import { CSSProperties } from "react";
+import ShowOnScrollIntoView from "../_components/showOnScrollIntoView";
+import { GoNorthStar } from "react-icons/go";
+
+const profileContainerStyle: CSSProperties = {
+  background: "linear-gradient(90deg, #3C58E3 -15%, #C2EBFF 58%, #5AB5E2 97%)",
+  overflow: "hidden"
+}
+
+const summaryText = "With over seven years of experience, I currently work at Nutanix in Bangalore, building modern web applications with React. I love creating smooth, responsive user experiences — and I’m especially passionate about writing code that’s scalable, high-performing, and easy to maintain. While front-end is where I thrive, I’ve also explored Azure cloud, done a bit of backend development, and I’m on a steady path toward becoming a full-stack developer. I enjoy learning new things and contributing to projects that make a difference.";
+
 function AboutMePage() {
-  return ( <div>
-    about me comes here
+  const renderProfilePhoto = () => {
+    return <GridBox className="h-fit">
+        <div style={ profileContainerStyle } className="flex justify-center items-center min-w-[200px] rounded-3xl">
+        <Image src={ AboutProfile .src } alt="Profile" width="320" height="334" className="relative top-3"/>
+      </div>
+    </GridBox>
+  }
+
+  const renderSummaryBox = () => {
+    return <div className="h-full flex flex-col gap-4 items-center max-w-[1000px]">
+      <span className="text-4xl md:text-6xl font-medium mb-4 block">
+        <ShowOnScrollIntoView>
+          <div className="flex gap-2 items-center">
+            <GoNorthStar className="text-secondary" />
+            <span className="block">Self summary</span>
+            <GoNorthStar className="text-secondary" />
+          </div>
+        </ShowOnScrollIntoView>
+      </span>
+      <GridBox>
+        <span className="text-4xl font-medium mb-4 block">Hardik Jain</span>
+        <span className="block">{ summaryText }</span>
+      </GridBox>
+    </div>
+  }
+
+  return ( <div className="py-8 px-8 max-w-screen-[1170px] grid grid-cols-3 gap-8">
+    <div className="col-span-4 md:col-span-1 flex justify-center">
+      { renderProfilePhoto() }
+    </div>
+    <div className="col-span-4 md:col-span-2 ">
+      { renderSummaryBox() }
+    </div>
   </div> );
 }
 
